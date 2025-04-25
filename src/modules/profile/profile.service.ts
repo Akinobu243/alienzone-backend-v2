ae987bfc4b94c38256d43224b07e46d4a405bee9
FiveNine <fiveninethedev@gmail.com>
2025-04-25 14:47:22 +0300

    fix aws access key id naming

--- src/modules/profile/profile.service.ts
+++ src/modules/profile/profile.service.ts
@@ -20,7 +20,7 @@ export class ProfileService {
   private s3 = new S3Client({
     region: process.env.AWS_REGION,
     credentials: {
-      accessKeyId: process.env.AWS_ACCESS_KEY,
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
       secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
     },
   });
