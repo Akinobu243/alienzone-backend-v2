a46ddec93184ebc90969bd20c70c50e452ac781b
Muhammad Ahmad <muhammad.ahmad8043@gmail.com>
2025-06-24 15:12:10 +0500

    chore: update seed temporarily

--- prisma/seeds/index.ts
+++ prisma/seeds/index.ts
@@ -9,22 +9,24 @@ import { seed as seedItems } from './items.seed';
 import { seed as seedDailyRewards } from './dailyRewards.seed';
 import { seed as seedHunts } from './hunts.seed';
 import { seed as seedStore } from './store.seed';
+import { seed as updateAvailability } from './update_availability.seed';
 
 const prisma = new PrismaClient();
 
 async function main() {
   try {
     console.log('Starting seed...');
-    await seedParts(prisma);
-    await seedQuests(prisma);
-    await seedCharacters(prisma);
-    await seedRaid(prisma);
-    await seedPacks(prisma);
-    await seedItems(prisma);
-    await seedDailyRewards(prisma);
-    await seedAlienPartGroups(prisma);
-    await seedHunts(prisma);
-    await seedStore(prisma);
+    // await seedParts(prisma);
+    // await seedQuests(prisma);
+    // await seedCharacters(prisma);
+    // await seedRaid(prisma);
+    // await seedPacks(prisma);
+    // await seedItems(prisma);
+    // await seedDailyRewards(prisma);
+    // await seedAlienPartGroups(prisma);
+    // await seedHunts(prisma);
+    // await seedStore(prisma);
+    await updateAvailability(prisma);
 
     console.log('Seed completed successfully');
   } catch (error) {
